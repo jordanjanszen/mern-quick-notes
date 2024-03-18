1e881a7ee2c55acf479a04c08dce1b7940ebabdb
Jordan Janszen <jordanjanszen@hotmail.com>
2024-03-19 10:54:52 +1100

    log in working

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,15 +1,15 @@
-import checkToken from "../../utilities/users-service"
+import { checkToken } from '../../utilities/users-service';
 
 export default function OrderHistoryPage() {
-    
-    async function handleCheckToken() {
-        const expDate = await checkToken();
-    }
-    
-    return (
-        <>
-        <h1>OrderHistoryPage</h1>
-        <button onClick={ handleCheckToken } >Check When My Login Expires</button>
-        </>
-    );
-};
+  async function handleCheckToken() {
+    const expDate = await checkToken();
+    console.log(expDate);
+  }
+
+  return (
+    <>
+      <h1>OrderHistoryPage</h1>
+      <button onClick={handleCheckToken}>Check When My Login Expires</button>
+    </>
+  );
+}
